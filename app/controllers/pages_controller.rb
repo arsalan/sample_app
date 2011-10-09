52197599e168aad926458bbef9304fb73ccbdfb4
Arsalan Ahmed <itsarsalan@yahoo.com>
2011-10-09 15:09:33 -0400

    finished static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,14 @@
 class PagesController < ApplicationController
   def home
+  	@title = "This is our Home!"
   end
 
   def contact
+  	@title = "Contact Us"
+  end
+
+  def about
+  	@title = "About Me"
   end
 
 end
